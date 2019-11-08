ce97cb6f9138427dc99b329c06980a6f028dd16d
Ant <ryu4979@gmail.com>
2019-11-09 01:07:24 +0900

    fix: IORefの削除

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@
 
 module Main where
 
-import Data.IORef
+import System.Directory
 import Control.Monad
 import Network.HTTP.Simple
 import Data.Maybe
@@ -11,21 +11,16 @@ import qualified Data.ByteString.Lazy as L
 
 main :: IO ()
 main = do
-  let url = "http://strimuer213p.hatenablog.com/"
+  let url = "http://strimuer213p.hatenablog.com"
 
   imageLinks <- scrapeURL url (attrs "src" "img")
   maybe (putStrLn "NG")
     (\imgs -> do
-      -- todo: IORefの排除
-      fileCnt <- newIORef 1
-
       forM_ imgs $ \imgLink -> do
+        fileCnt <- length <$> listDirectory "outputs"
+        let saveFilePath = "./outputs/" ++ (show fileCnt) ++ ".png"
+
         res <- httpLBS $ parseRequest_ imgLink
-
-        fileCnt' <- readIORef fileCnt
-        let saveFilePath = "./outputs/" ++ (show fileCnt') ++  ".png"
         L.writeFile (saveFilePath) (getResponseBody res)
-
         putStrLn $ "Save: " ++ saveFilePath
-        modifyIORef fileCnt (+1)
     ) imageLinks
